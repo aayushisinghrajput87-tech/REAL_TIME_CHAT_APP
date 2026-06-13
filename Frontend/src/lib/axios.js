029625da3b0d348d82afae88c8f03cf4a1131862
manish <manishkr061210@gmail.com>
2026-06-13 21:58:05 +0530

    fix url

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:"https://real-time-chat-app-d9ie.onrender.com/api",
     withCredentials:true,
 });
