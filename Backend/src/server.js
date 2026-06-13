3567263e9d83b9825b7596e541916c731d803b49
manish <manishkr061210@gmail.com>
2026-06-13 22:09:24 +0530

    fix

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,7 +13,7 @@ const app=express();
 const PORT=process.env.PORT;
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174","https://real-time-chat-app-tawny-phi.vercel.app/"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://real-time-chat-app-tawny-phi.vercel.app"],
     credentials: true,
 }));
 app.use(express.json());
